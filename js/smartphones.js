2af46fe93ee758dcfbea77b28ea997289c81bb65
opr97 <orlandopasaca@gmail.com>
2020-10-03 21:19:35 -0500

    Se implemento el template productos a smarthphones.html y se corrigio algunas peticiones

--- js/smartphones.js
+++ js/smartphones.js
@@ -1,8 +1,9 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 var productos;
 var isVisible = false;
 var getProductos = function (limit) {
-    return fetch("https://hiddensoft.net/pruebasconsultas/tienda_ivco/catalogo.php?&limit=" + limit)
+    return fetch("https://hiddensoft.net/pruebasconsultas/tienda_ivco/catalogo.php?tipo=smartphone&limit=" + limit)
         .then(function (response) { return response.json(); })
         .catch(function (err) { return productos = err; });
 };
