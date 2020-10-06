862681516d647b6f7542e51863bdb6f20f8ee293
opr97 <orlandopasaca@gmail.com>
2020-10-05 21:03:30 -0500

    Se agrego SDK de Facebook y se probara en git

---
+++ js/facebook.sdk.js
@@ -0,0 +1,24 @@
+
+window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '2897775743838475',
+      cookie     : true,
+      xfbml      : true,
+      version    : 'v8.0'
+    });
+      
+    FB.AppEvents.logPageView();   
+    FB.getLoginStatus((response)=>
+{
+    console.log(response);
+});
+  };
+
+  (function(d, s, id){
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+
