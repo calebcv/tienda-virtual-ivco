60babb1c4d36808d21e3886928e0054b8dd6512f
opr97 <orlandopasaca@gmail.com>
2020-10-06 19:54:17 -0500

    Prueba face sdk 20

--- js/cuenta.js
+++ js/cuenta.js
@@ -3,16 +3,18 @@ var dataUser;
 var contDataUser = document.getElementsByClassName("cont_dataUser");
 var verificarDataUser = function () {
     if (localStorage.getItem("dataUser")) {
+        console.log("si hay storage");
         dataUser = localStorage.getItem("dataUser");
         dataUser = JSON.parse(dataUser);
         insertDomDataUser();
     }
-    else
+    else {
+        console.log("si hay storage");
         while (contDataUser[0].firstChild)
             contDataUser[0].removeChild(contDataUser[0].firstChild);
+    }
 };
 var insertDomDataUser = function () {
-    var contDataUser = document.getElementsByClassName("cont_dataUser");
     var newDivPictureUser = document.createElement("img");
     newDivPictureUser.src = dataUser.picture.data.url;
     contDataUser[0].appendChild(newDivPictureUser);
