e174fd91536392a2e4d9a7ecd140899c9cdabb60
opr97 <orlandopasaca@gmail.com>
2020-10-07 15:09:56 -0500

    Prueba sdk 2

--- js/cuenta.js
+++ js/cuenta.js
@@ -1,17 +1,23 @@
 "use strict";
 var dataUser;
 var contDataUser = document.getElementsByClassName("cont_dataUser");
+var titleForm = document.getElementsByClassName("title-form");
 var verificarDataUser = function () {
-    while (contDataUser[0].firstChild)
-            contDataUser[0].removeChild(contDataUser[0].firstChild);
     if (localStorage.getItem("userData")) {
         console.log("si hay storage");
         dataUser = localStorage.getItem("userData");
         dataUser = JSON.parse(dataUser);
         insertDomDataUser();
     }
+    else {
+        console.log("no hay storage");
+        titleForm[0].innerHTML = "Iniciar Sesión";
+        while (contDataUser[0].firstChild)
+            contDataUser[0].removeChild(contDataUser[0].firstChild);
+    }
 };
 var insertDomDataUser = function () {
+    titleForm[0].innerHTML = "Tu cuenta";
     var newDivPictureUser = document.createElement("img");
     newDivPictureUser.className = "img-fluid col-2";
     newDivPictureUser.src = dataUser.picture.data.url;
@@ -20,9 +26,9 @@ var insertDomDataUser = function () {
     var newPName = document.createElement("p");
     newPName.innerHTML = "Nombre: " + dataUser.first_name + " " + dataUser.last_name;
     var newPEmail = document.createElement("p");
-    newPEmail.innerHTML = "Email: " + dataUser.email;
+    newPName.innerHTML = "Email: " + dataUser.email;
     var newPID = document.createElement("p");
-    newPID.innerHTML = "ID: " + dataUser.id;
+    newPName.innerHTML = "ID: " + dataUser.id;
     contDataUser[0].appendChild(newDivPictureUser);
     contDataUser[0].appendChild(newContData);
     newContData.appendChild(newPName);
