f9bd7e1deb3bda6e3008235d4af3d2b07bf061bf
opr97 <orlandopasaca@gmail.com>
2020-10-07 15:50:41 -0500

    Prueba sdk 8

--- js/cuenta.js
+++ js/cuenta.js
@@ -3,23 +3,20 @@ var dataUser;
 var contDataUser = document.getElementsByClassName("cont_dataUser");
 var titleForm = document.getElementsByClassName("title-form");
 var verificarDataUser = function () {
+    titleForm[0].innerHTML = "Iniciar Sesión";
     while (contDataUser[0].firstChild)
-            contDataUser[0].removeChild(contDataUser[0].firstChild);
+        contDataUser[0].removeChild(contDataUser[0].firstChild);
     if (localStorage.getItem("userData")) {
         console.log("si hay storage");
         dataUser = localStorage.getItem("userData");
         dataUser = JSON.parse(dataUser);
         insertDomDataUser();
     }
-    else {
-        console.log("no hay storage");
-        titleForm[0].innerHTML = "Iniciar Sesión";
-    }
 };
 var insertDomDataUser = function () {
     titleForm[0].innerHTML = "Tu cuenta";
     var newDivPictureUser = document.createElement("img");
-    newDivPictureUser.className = "img-fluid col-3 p-0";
+    newDivPictureUser.className = "img-fluid col-2";
     newDivPictureUser.src = dataUser.picture.data.url;
     var newContData = document.createElement("div");
     newContData.className = "col-8";
